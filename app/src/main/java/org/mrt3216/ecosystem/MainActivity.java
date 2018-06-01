338ab4b65f5e8e4c31f5cf537e2ef75a6372f6b1
Braeden Hunt <20hunbra@student.mcpsmt.org>
2018-06-01 09:49:48 -0600

    Initial commit

---
+++ app/src/main/java/org/mrt3216/ecosystem/MainActivity.java
@@ -0,0 +1,34 @@
+package org.mrt3216.ecosystem;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity {
+    private TextView biotic, abiotic;
+    private ImageView image;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        biotic = findViewById(R.id.button1);
+        abiotic = findViewById(R.id.button2);
+        image = findViewById(R.id.imageView);
+
+        image.setImageResource(R.drawable.tundra);
+
+        biotic.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(), ListViewer.class);
+                startActivity(intent);
+            }
+        });
+    }
+}
